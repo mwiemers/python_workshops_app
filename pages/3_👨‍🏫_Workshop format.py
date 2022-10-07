b8954a1df5929ba22a6b22812953cf4ef8b8354c
mwiemers84 <59834672+mwiemers84@users.noreply.github.com>
2022-10-07 13:56:08 +0100

    cchanged text for workshop format

--- pages/3_👨‍🏫_Workshop format.py
+++ pages/3_👨‍🏫_Workshop format.py
@@ -19,12 +19,20 @@ st.markdown("""
     and in-application Help features to find the solutions to real-world problems, with a **Python expert** on hand if you get stuck.
 
     ### What to do when you get stuck:
-    - Ask your fellow students
+
+    On Dataquest you have the option to see the solution for each exercise. We highly recommend to only use that as a last resort when you 
+    have explored all other options. Being able to search effectively online, identify relevant resources and use them to fix your code 
+    are very important skills to enable you to continue to learn independently.
+
+    If you get stuck, we recommend to:
+
+    - Ask one of our Python experts for help
     - Search online (here are a couple of useful resources):
         - The answer box on the top of Google's results page
         - [Stackoverflow](https://stackoverflow.com) (for task specific solutions)
         - [RealPython](https://realpython.com) (for topic based tutorials)
         - [w3schools](https://w3schools.com]) (for short task specific tutorials and examples)
-    - Ask one of our Python experts for help
+    - Ask your fellow students
+    
     """
 )
