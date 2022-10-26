d9ba0489442bd5f508e1b5358cdbc74b03c979fc
mwiemers <michael.wiemers84@gmail.com>
2022-10-26 12:21:50 +0100

    changed format page

--- pages/3_👨‍🏫_Workshop format.py
+++ pages/3_👨‍🏫_Workshop format.py
@@ -11,11 +11,18 @@ st.title("The Python Workshop Format")
 st.markdown("""
     
     In the Python for Data Science workshop series you will be learning Python through the online learning platform **Dataquest**. 
-    
-    You will have **complete flexibility** to choose courses from Dataquest based on your personal learning goals and switch between 
-    online self-study and supported in-person learning on-campus depending on your learning preferences and changing schedule.
 
-    Each workshop is **one-hour** long, and you will work **with fellow learners**, utilising your **prior experience**, **web searches**, 
+    ### Why do we choose this format?
+
+    By learning Python with Dataquest, you as the learner can:
+    
+    - choose freely what to learn
+    - progress much further as when using in-house developed materials
+    - switch between independent online and guided in-person learning depending on their schedule, availability and personal preference
+    - work on a collection of projects provided on Dataquest to further consolidate and deepen their learning which are also a great way to demonstrate their coding skills to employers
+    - download certificates from dataquest and append these to your social media profiles (LinkedIN) to provide evidence of your learning
+    
+    Each workshop is **two-hours** long, and you will work **with fellow learners**, utilising your **prior experience**, **web searches**, 
     and in-application Help features to find the solutions to real-world problems, with a **Python expert** on hand if you get stuck.
 
     ### What to do when you get stuck:
