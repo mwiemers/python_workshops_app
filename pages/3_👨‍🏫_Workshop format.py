cfcd7df0040381ce47c387713c9dbc62303ab4b2
Michael Wiemers <m.wiemers@lse.ac.uk>
2023-01-12 15:10:11 +0000

    updated pages

--- pages/3_👨‍🏫_Workshop format.py
+++ pages/3_👨‍🏫_Workshop format.py
@@ -9,21 +9,54 @@ st.set_page_config(
 st.title("The Python Workshop Format")
 
 st.markdown("""
-    
-    In the Python for Data Science workshop series you will be learning Python through the online learning platform **Dataquest**. 
 
-    ### Why do we choose this format?
+    # DSL Python workshops
 
-    By learning Python with Dataquest, you as the learner can:
-    
-    - choose freely what to learn
-    - progress much further as when using in-house developed materials
-    - switch between independent online and guided in-person learning depending on your schedule, availability and personal preference
-    - work on a collection of projects provided on Dataquest to further consolidate and deepen your learning which is a great way to demonstrate your Python skills to employers
-    - download certificates from dataquest and append these to your social media profiles (LinkedIN) to provide evidence of your learning
-    
+    Our Python workshops are split into two type of sessions:
+    1) Python Introduction
+    2) Python for Data Science
+
+    Both of these workshops are open-format, which means that you can choose which topic to work on. It is advised to work through the materials 
+    in order unless you are already familiar with a particular topic.
+
     Each workshop is **two-hours** long, and you will work **with fellow learners**, utilising your **prior experience**, **web searches**, 
     and in-application Help features to find the solutions to real-world problems, with a **Python expert** on hand if you get stuck.
+
+    ## Python Introduction
+    The Python Introduction workshops are for beginners that have no prior experience in programming with Python and cover the following topics:
+    Python Fundamentals:
+    - Numerical variables
+    - String variables
+    - Type casting
+    - Lists
+
+    ## Python for Data Science
+    Once you have completed the materials from the Python Introduction series or in case you are already familiar with these topics, feel free 
+    to move on to the Python for Data Science series where you will learn about more complex fundamental programming concepts and how to 
+    use libraries to manipulate and visualize data in Python.
+
+    The Python for Data Science workshops cover the following topics:
+    Python Fundamentals:
+    - List of lists
+    - Functions and modules
+    - For loops
+    - Conditionals
+    - Writing functions
+    - Guided Project: Zoopla Rent Prices
+
+    Python Data Wrangling:
+    - NumPy arrays
+    - NumPy 2D arrays
+    - Pandas Basics
+    - Pandas Indexing
+    - Pandas Exploring Data
+
+    Python Data Visualization
+    - Line plots with matplotlib
+    - Scatter, Bar, Histogram with Matplotlib
+    - Seaborn and Pandas plots
+
+
 
     ### What to do when you get stuck:
 
